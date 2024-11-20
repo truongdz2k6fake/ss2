4f32697246ddc801fe2346a035011cd7e2774572
truongdz2k6fake <aduvip11111@gmail.com>
2024-11-21 00:55:29 +0700

    Add files via upload

---
+++ bai 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	 // kieu du lieu so nguyen 
+	 int minh_map_80kg;
+	 // kieu du lieu so thuc
+	 float tron_lang;
+	 double phon_luong;
+	 // kieu du lieu ki tu 
+	 char son_lon;
+
+	return 0;
+} 
