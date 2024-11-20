1160e6e89bad00d3cb3e72131393c8fa6f431dbb
truongdz2k6fake <aduvip11111@gmail.com>
2024-11-21 00:57:23 +0700

    Add files via upload

---
+++ bai 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	  int a = 7;
+	  printf("canh hinh vuong la : %d \n",a);
+	  int b = 4 * a;
+	  printf("chu vi hinh vuong la : %d \n",b);
+	  int c = a * a;
+	  printf("dien tich hinh vuong la : %d \n",c);
+	
+	return 0;
+} 
