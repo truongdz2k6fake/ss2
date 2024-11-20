33a6480269d83a368ed85df952fab1a8a481fafc
truongdz2k6fake <aduvip11111@gmail.com>
2024-11-21 00:56:39 +0700

    Add files via upload

---
+++ bai 3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	  int a = 12,b = 4;
+	 printf("so thu nhat la : %d \n",a);
+	 printf("so thu hai la : %d \n",b);
+	 int c = a + b;
+	 printf("tong hai so la : %d \n",c);
+	 int d = a - b;
+	 printf("hieu hai so la : %d \n",d);
+	 int e = a * b;
+	 printf("tich hai so la : %d \n",e);
+	 float f = a / b;
+	 printf("thuong hai so la : %.f \n",f);
+	 
+	return 0;
+} 
