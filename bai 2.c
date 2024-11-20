9132ca4692b4767f6a621e199f67fe37b4ada19b
truongdz2k6fake <aduvip11111@gmail.com>
2024-11-21 00:55:46 +0700

    Add files via upload

---
+++ bai 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	 int a = 3,b = 4;
+	 printf("so thu nhat la : %d \n",a);
+	 printf("so thu hai la : %d \n",b);
+	 int c = a + b;
+	 printf("tong hai so la : %d \n",c);
+	
+	return 0;
+} 
