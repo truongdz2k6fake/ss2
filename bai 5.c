a4e28c1da924f65585a45f995d12a25bb19b59be
truongdz2k6fake <aduvip11111@gmail.com>
2024-11-21 00:57:41 +0700

    Add files via upload

---
+++ bai 5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	  int a = 7 , b = 3;
+	  printf("chieu dai la : %d \n",a);
+	  printf("chieu rong la : %d \n",b);
+	  int c = ( a + b ) * 2;
+	  printf("chu vi la : %d \n",c);
+	  int d = a * b;
+	  printf("dien tich la : %d \n",d);
+	
+	return 0;
+} 
